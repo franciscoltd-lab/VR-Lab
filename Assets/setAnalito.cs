676808d164c30fb661fbfb00b2a45eb9fa45ebd1
Javier Reyna <freyna@blen.com.mx>
2024-03-05 23:24:44 -0600

    Refactor: Limpiando código

--- Assets/setAnalito.cs
+++ Assets/setAnalito.cs
@@ -17,20 +17,6 @@ public class setAnalito : MonoBehaviour
     private TextMeshProUGUI[] infoTextTitulante;
 
     private string url = "http://192.168.1.72:5000/analitos/1";
-    void Start()
-    {
-
-        //toggle.onValueChanged.AddListener(delegate
-        //{
-        //    getAnalito(toggle);
-        //});
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public async void getAnalito(Toggle change)
     {
@@ -44,28 +30,28 @@ public class setAnalito : MonoBehaviour
                     // Realiza una solicitud GET al endpoint
                     HttpResponseMessage response = await client.GetAsync(url);
 
-                    // Verifica si la solicitud fue exitosa (código de estado 200)
+                    // Verifica si la solicitud fue exitosa (cï¿½digo de estado 200)
                     if (response.IsSuccessStatusCode)
                     {
                         // Lee el contenido de la respuesta en formato JSON
                         string responseBody = await response.Content.ReadAsStringAsync();
                         AnalitoInfo analitoInfo = JsonConvert.DeserializeObject<AnalitoInfo>(responseBody);
 
-                        //Llenar la información con el analito seleccionado
+                        //Llenar la informaciï¿½n con el analito seleccionado
                         infoTextAnalito = infoCardAnalito.GetComponentsInChildren<TextMeshProUGUI>();
                         infoTextAnalito[1].text = analitoInfo.nombre_analito;
                         infoTextAnalito[4].text = "Estructura: " + analitoInfo.estructura_analito;
 
-                        //Llenar la información con el analito seleccionado
+                        //Llenar la informaciï¿½n con el analito seleccionado
                         infoTextTitulante = infoCardTitulante.GetComponentsInChildren<TextMeshProUGUI>();
                         infoTextTitulante[1].text = analitoInfo.nombre_titulante;
-                        infoTextTitulante[2].text = "Concentración Real: " + analitoInfo.concentracion_titulante + " N";
+                        infoTextTitulante[2].text = "Concentraciï¿½n Real: " + analitoInfo.concentracion_titulante + " N";
                         infoTextTitulante[3].text = "Estructura: " + analitoInfo.estructura_titulante;
 
                     }
                     else
                     {
-                        // Si la solicitud no fue exitosa, imprime el código de estado
+                        // Si la solicitud no fue exitosa, imprime el cï¿½digo de estado
                         Console.WriteLine($"Error al realizar la solicitud: {response.StatusCode}");
                     }
                 }
